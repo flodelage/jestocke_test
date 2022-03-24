97ff32000a63e861ea0471bc689a90bc2a975617
Florian Delage <delageflo@ymail.com>
2022-03-24 13:28:24 +0100

    num_to_roman() can convert 1 to 9

--- num_converters.py
+++ num_converters.py
@@ -4,4 +4,11 @@ def num_to_roman(num:int) -> str:
     converts classical numbers (int) into roman numbers (str)
     example: 5 -> 'V'
     """
-    pass
+    roman_nbs = [
+        ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+        ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+        ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+    ]
+
+    if len(str(num)) == 1:
+        return roman_nbs[0][num-1]
