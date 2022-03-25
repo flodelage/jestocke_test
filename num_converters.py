eec423a36ec4b2506dba9631a4604e4b83bfdac5
Florian Delage <delageflo@ymail.com>
2022-03-25 17:25:24 +0100

    add RomanNumeralConverter class

--- num_converters.py
+++ num_converters.py
@@ -1,45 +1,46 @@
 
-def num_to_roman(num: int) -> str:
-    """
-    converts classical numbers (int) into roman numbers (str)
-    example: 5 -> 'V'
-    """
-    roman_nbs = [
-        ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
-        ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
-        ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
-    ]
+class RomanNumeralConverter:
 
-    result = ""
-    for turn, n in enumerate(str(num), start=1):
-        if n == '0':
-            next
-        else:
-            result += roman_nbs[len(str(num))-turn][int(n)-1]
-    return result
+    def num_to_roman(self, num: int) -> str:
+        """
+        converts classical numbers (int) into roman numbers (str)
+        example: 5 -> 'V'
+        """
+        roman_nbs = [
+            ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+            ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+            ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+        ]
 
-
-def roman_to_num(rom: str) -> int:
-    """
-    converts roman numbers (str) into classical numbers (int)
-    example: 'V' -> 5
-    """
-    classical_nbs = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
-        'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900
-    }
-
-    result = 0
-    turn = 0
-    while turn < len(rom):
-        try:
-            if rom[turn] + rom[turn+1] in classical_nbs:
-                result += classical_nbs[rom[turn] + rom[turn+1]]
-                turn += 2
+        result = ""
+        for turn, n in enumerate(str(num), start=1):
+            if n == '0':
+                next
             else:
+                result += roman_nbs[len(str(num))-turn][int(n)-1]
+        return result
+
+    def roman_to_num(self, rom: str) -> int:
+        """
+        converts roman numbers (str) into classical numbers (int)
+        example: 'V' -> 5
+        """
+        classical_nbs = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+            'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900
+        }
+
+        result = 0
+        turn = 0
+        while turn < len(rom):
+            try:
+                if rom[turn] + rom[turn+1] in classical_nbs:
+                    result += classical_nbs[rom[turn] + rom[turn+1]]
+                    turn += 2
+                else:
+                    result += classical_nbs[rom[turn]]
+                    turn += 1
+            except IndexError:
                 result += classical_nbs[rom[turn]]
                 turn += 1
-        except IndexError:
-            result += classical_nbs[rom[turn]]
-            turn += 1
-    return result
+        return result
--- tests/test_num_converters.py
+++ tests/test_num_converters.py
@@ -1,76 +1,82 @@
 
 import unittest
 
-from num_converters import num_to_roman, roman_to_num
+from num_converters import RomanNumeralConverter
 
 
 class TestConvertersNumToRoman(unittest.TestCase):
 
+    def setUp(self):
+        self.conv = RomanNumeralConverter()
+
     def test_num_to_roman_5(self):
-        self.assertEqual(num_to_roman(5), 'V')
+        self.assertEqual(self.conv.num_to_roman(5), 'V')
 
     def test_num_to_roman_15(self):
-        self.assertEqual(num_to_roman(15), 'XV')
+        self.assertEqual(self.conv.num_to_roman(15), 'XV')
 
     def test_num_to_roman_20(self):
-        self.assertEqual(num_to_roman(20), 'XX')
+        self.assertEqual(self.conv.num_to_roman(20), 'XX')
 
     def test_num_to_roman_30(self):
-        self.assertEqual(num_to_roman(30), 'XXX')
+        self.assertEqual(self.conv.num_to_roman(30), 'XXX')
 
     def test_num_to_roman_40(self):
-        self.assertEqual(num_to_roman(40), 'XL')
+        self.assertEqual(self.conv.num_to_roman(40), 'XL')
 
     def test_num_to_roman_50(self):
-        self.assertEqual(num_to_roman(50), 'L')
+        self.assertEqual(self.conv.num_to_roman(50), 'L')
 
     def test_num_to_roman_55(self):
-        self.assertEqual(num_to_roman(55), 'LV')
+        self.assertEqual(self.conv.num_to_roman(55), 'LV')
 
     def test_num_to_roman_60(self):
-        self.assertEqual(num_to_roman(60), 'LX')
+        self.assertEqual(self.conv.num_to_roman(60), 'LX')
 
     def test_num_to_roman_70(self):
-        self.assertEqual(num_to_roman(70), 'LXX')
+        self.assertEqual(self.conv.num_to_roman(70), 'LXX')
 
     def test_num_to_roman_80(self):
-        self.assertEqual(num_to_roman(80), 'LXXX')
+        self.assertEqual(self.conv.num_to_roman(80), 'LXXX')
 
     def test_num_to_roman_90(self):
-        self.assertEqual(num_to_roman(90), 'XC')
+        self.assertEqual(self.conv.num_to_roman(90), 'XC')
 
     def test_num_to_roman_100(self):
-        self.assertEqual(num_to_roman(100), 'C')
+        self.assertEqual(self.conv.num_to_roman(100), 'C')
 
     def test_num_to_roman_500(self):
-        self.assertEqual(num_to_roman(500), 'D')
+        self.assertEqual(self.conv.num_to_roman(500), 'D')
 
     def test_num_to_roman_538(self):
-        self.assertEqual(num_to_roman(538), 'DXXXVIII')
+        self.assertEqual(self.conv.num_to_roman(538), 'DXXXVIII')
 
     def test_num_to_roman_807(self):
-        self.assertEqual(num_to_roman(807), 'DCCCVII')
+        self.assertEqual(self.conv.num_to_roman(807), 'DCCCVII')
 
     def test_num_to_roman_999(self):
-        self.assertEqual(num_to_roman(999), 'CMXCIX')
+        self.assertEqual(self.conv.num_to_roman(999), 'CMXCIX')
 
 
 class TestConvertersRomanToNum(unittest.TestCase):
 
+    def setUp(self):
+        self.conv = RomanNumeralConverter()
+
     def test_roman_to_num_5(self):
-        self.assertEqual(roman_to_num('V'), 5)
+        self.assertEqual(self.conv.roman_to_num('V'), 5)
 
     def test_roman_to_num_15(self):
-        self.assertEqual(roman_to_num('XV'), 15)
+        self.assertEqual(self.conv.roman_to_num('XV'), 15)
 
     def test_roman_to_num_20(self):
-        self.assertEqual(roman_to_num('XX'), 20)
+        self.assertEqual(self.conv.roman_to_num('XX'), 20)
 
     def test_roman_to_num_500(self):
-        self.assertEqual(roman_to_num('D'), 500)
+        self.assertEqual(self.conv.roman_to_num('D'), 500)
 
     def test_roman_to_num_807(self):
-        self.assertEqual(roman_to_num('DCCCVII'), 807)
+        self.assertEqual(self.conv.roman_to_num('DCCCVII'), 807)
 
     def test_roman_to_num_999(self):
-        self.assertEqual(roman_to_num('CMXCIX'), 999)
+        self.assertEqual(self.conv.roman_to_num('CMXCIX'), 999)
